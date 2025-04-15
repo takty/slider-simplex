5fb3b302cb9568509d2a135b073b40d72f826305
Takuto Yanagida <tokutadaginaya@gmail.com>
2025-04-15 22:15:22 +0900

    Update entry points to use CSS instead of SCSS and add slider-simplex.css

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ import { build } from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
 
 await build({
-	entryPoints : ['src/slider-simplex.ts', 'src/slider-simplex.scss'],
+	entryPoints : ['src/slider-simplex.ts', 'src/slider-simplex.css'],
 	plugins     : [sassPlugin()],
 	outdir      : 'dist',
 	outExtension: { '.js': '.min.js', '.css': '.min.css' },
