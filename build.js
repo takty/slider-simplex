37309f717aa53917832998765829370f088c60eb
Takuto Yanagida <tokutadaginaya@gmail.com>
2025-04-16 21:52:35 +0900

    Remove sass packages

--- build.js
+++ build.js
@@ -1,9 +1,7 @@
 import { build } from 'esbuild';
-import { sassPlugin } from 'esbuild-sass-plugin';
 
 await build({
 	entryPoints : ['src/slider-simplex.ts', 'src/slider-simplex.css'],
-	plugins     : [sassPlugin()],
 	outdir      : 'dist',
 	outExtension: { '.js': '.min.js', '.css': '.min.css' },
 	minify      : true,
